2cf7bbd9ce033d7a59b9d92c6f37ab3efa60d612
saurabh yadav <ysaurabh117@gmail.com>
2021-03-05 16:20:55 +0530

    first deploy

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const { dirname } = require('@angular/compiler-cli/src/ngtsc/file_system');
-const express = require('express');
+// const { dirname } = require('@angular/compiler-cli/src/ngtsc/file_system');
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static("./public  "));
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/navbar/index.html'));
+// });
+// app.listen(process.env.PORT || 8080);
 const path = require('path');
-const app = express();
-app.use(express.static("./public  "));
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/navbar/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-app.listen(process.env.PORT || 8080);
