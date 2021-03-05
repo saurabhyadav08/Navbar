5c9a7e6632d7319f85561f4cbad6d8aa8f9abebd
saurabh yadav <ysaurabh117@gmail.com>
2021-03-05 16:33:04 +0530

    somethings

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-// const { dirname } = require('@angular/compiler-cli/src/ngtsc/file_system');
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// app.use(express.static("./public  "));
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/navbar/index.html'));
-// });
-// app.listen(process.env.PORT || 8080);
+const { dirname } = require('@angular/compiler-cli/src/ngtsc/file_system');
+const express = require('express');
 const path = require('path');
-// ...
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
+const app = express();
+app.use(express.static("./public  "));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/navbar/index.html'));
 });
+app.listen(process.env.PORT || 8080);
